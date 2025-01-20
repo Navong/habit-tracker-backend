8b437cc8141d63f8f2e41eee45b9cf803da84adc
Navong <bongchannavong68@gmail.com>
2025-01-21 05:57:00 +0900

    fix

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+//cors
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+  
+app.use(express.json());
 
 // Journal Routes
 app.get("/api/journals", async (req, res) => {
