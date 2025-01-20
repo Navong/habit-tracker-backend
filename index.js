5b23045584ad0eb309a261361e5f15b76531ccb5
Navong <bongchannavong68@gmail.com>
2025-01-21 05:23:31 +0900

    fix

--- index.js
+++ index.js
@@ -14,10 +14,55 @@ app.use(cors({ origin: "*" }));
 app.use("/api/habits", habitRoutes);
 app.use("/api/journals", journalRoutes);
 
+// Habit Route
+
+app.get("/api/habits", async (req, res) => {
+    try {
+        const habits = await prisma.habit.findMany();
+        res.json(habits);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching habits" });
+    }
+})
+
+app.post("/api/habits", async (req, res) => {
+    const { name, description, goal, frequency, color, category } = req.body;
+    try {
+        const newHabit = await prisma.habit.create({
+            data: { name, description, goal, frequency, color, completedDates: [], category },
+        });
+        console.log(newHabit);
+        res.status(201).json(newHabit);
+    } catch (error) {
+        res.status(500).json({ error: "Error creating habit" });
+    }
+});
+
+app.put("/api/habits/:id", async (req, res) => {
+    const { id } = req.params;
+    const data = req.body;
+    try {
+        const updatedHabit = await prisma.habit.update({ where: { id }, data });
+        res.json(updatedHabit);
+    } catch (error) {
+        res.status(500).json({ error: "Error updating habit" });
+    }
+});
+
+app.delete("/api/habits/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await prisma.habit.delete({ where: { id } });
+        res.json({ message: "Habit deleted" });
+    } catch (error) {
+        res.status(500).json({ error: "Error deleting habit" });
+    }
+});
 
 
-const PORT = process.env.PORT || 4000;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
 
